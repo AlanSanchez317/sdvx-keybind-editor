94cdc2b4efd35d7594701298515ae0d908d1bff5
DerpNerd <asanch317@west-mec.org>
2024-05-02 15:26:38 -0700

    pls

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const robot = require('robotjs');
+
+// Implement functions for selecting the controller and managing keybinds
+
+function setKeybinds(controller) {
+    // Map controller buttons to keyboard keys using robotjs or Node HID
+}
